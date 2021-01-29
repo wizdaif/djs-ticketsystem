7c9565052e9343662b70fda590d41d4bbe622563
GHOST <ghostdevbusiness@gmail.com>
2021-01-29 11:43:35 +0000

    feat: check that discord.js is installed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
+try {
+    require('discord.js');
+} catch {
+    throw new Error('Unable to find discord.js, please install it.');
+}
+
 module.exports = {
     TicketSystem: require('./Core/TicketSystem.js'),
 };
